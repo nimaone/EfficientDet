d2c9bcd3aa317fdf660b60dc69dea2099a4f2c95
nimaone <58114984+nimaone@users.noreply.github.com>
2020-12-30 17:40:44 +0330

    Update layers.py

--- layers.py
+++ layers.py
@@ -232,7 +232,7 @@ def filter_detections(
         ratios = keras.backend.gather(ratios, indices)
         alphas = tf.pad(alphas, [[0, pad_size], [0, 0]], constant_values=-1)
         ratios = tf.pad(ratios, [[0, pad_size]], constant_values=-1)
-        alphas.set_shape([max_detections, 4])
+        alphas.set_shape([max_detections, 2])
         ratios.set_shape([max_detections])
         return [boxes, scores, alphas, ratios, labels]
     else:
@@ -341,7 +341,7 @@ class FilterDetections(keras.layers.Layer):
             return [
                 (input_shape[0][0], self.max_detections, 4),
                 (input_shape[1][0], self.max_detections),
-                (input_shape[1][0], self.max_detections, 4),
+                (input_shape[1][0], self.max_detections, 2),
                 (input_shape[1][0], self.max_detections),
                 (input_shape[1][0], self.max_detections),
             ]
